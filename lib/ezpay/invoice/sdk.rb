f089e18c22c1c7fd5e5b700b5f6fbc32764a7d4a
Whyayen <c75a90@gmail.com>
2022-01-09 22:27:37 +0800

    Init project

---
+++ lib/ezpay/invoice/sdk.rb
@@ -0,0 +1,10 @@
+require "ezpay/invoice/sdk/version"
+
+module Ezpay
+  module Invoice
+    module Sdk
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ezpay/invoice/sdk/version.rb
@@ -0,0 +1,7 @@
+module Ezpay
+  module Invoice
+    module Sdk
+      VERSION = "0.1.0"
+    end
+  end
+end
